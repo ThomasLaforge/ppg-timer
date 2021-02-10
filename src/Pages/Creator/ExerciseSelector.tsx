526d33950d330c18c18875718cb8cb2fd6ece0a4
Thomas Laforge <thomas.laforge.38@gmail.com>
2021-02-10 01:17:46 +0100

    fix exercise selector

--- src/Pages/Creator/ExerciseSelector.tsx
+++ src/Pages/Creator/ExerciseSelector.tsx
@@ -2,15 +2,21 @@ import { MenuItem, Select } from "@material-ui/core"
 import React, { useMemo, useState } from "react"
 import { exerciseDB } from "../../database/index"
 
+interface ExerciseSelectorProps {
+    value: number,
+    onChange: (event: React.ChangeEvent<{
+        name?: string;
+        value: string;
+    }>, child: React.ReactNode) => void
+}
 
-export default function ExerciseSelector(){
-    const [exerciseId, setExerciseId] = useState(0)
+export default function ExerciseSelector(props: ExerciseSelectorProps){
     const exerciceList = useMemo(() => exerciseDB.getExerciseList(), [])
 
     return <Select
         className="exercise-selector exercise-input"
-        value={exerciseId}
-        onChange={(e) => setExerciseId(e.target.value as number)}
+        value={props.value}
+        onChange={props.onChange}
     >
         {exerciceList.map( (e, k) => {
             return <MenuItem key={k} value={k}>{e.name}</MenuItem>
