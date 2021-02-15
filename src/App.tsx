9dde583d2e9d913199454732ac5237c018d2bd56
Thomas Laforge <thomas.laforge.38@gmail.com>
2021-02-15 17:51:43 +0100

    new material ui theme

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
 import Home from './Pages/Home/index'
 import Creator from './Pages/Creator/index'
 import { IconButton, Toolbar, AppBar, Drawer, List, ListItem, ListItemText, Typography, ListItemIcon } from '@material-ui/core';
-
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import HomeIcon from '@material-ui/icons/Home';
 import Executor from './Pages/Executor';
@@ -29,57 +29,66 @@ if (firebase.apps.length === 0) {
 
 const api = firebase.firestore();
 export const trainingsAPI = api.collection("trainings")
-// trainingsCollection.get().then((querySnapshot: any) => {
-//   querySnapshot.forEach((doc: any) => {
-//       console.log(doc.data());
-//   });
-// });
 
+
+export const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: 'rgba(5,15,62,0.94)',
+    },
+    secondary: {
+      main: '#fad936',
+    },
+  },
+});
 
 function App() {
   const [drawerOpen, setdrawerOpen] = React.useState(false)
 
   return (
     <HashRouter>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" color="inherit" aria-label="menu" onClick={() => setdrawerOpen(!drawerOpen)}>
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6">
-            PPG Timer
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton edge="start" color="secondary" aria-label="menu" onClick={() => setdrawerOpen(!drawerOpen)}>
+              <MenuIcon />
+            </IconButton>
+            <Typography color='secondary' variant="h6" >
+              PPG Timer
+            </Typography>
+          </Toolbar>
+        </AppBar>
 
-      <Drawer anchor={'left'} open={drawerOpen} onClose={() => setdrawerOpen(false)}>
-        <List>
-          <ListItem button key={0}>
-              <Link to='/'>
-                <ListItemIcon><HomeIcon /></ListItemIcon>
-                <ListItemText primary={'Home'} />
+        <Drawer anchor={'left'} open={drawerOpen} onClose={() => setdrawerOpen(false)}>
+          <List>
+            <ListItem button key={0}>
+                <Link to='/'>
+                  <ListItemIcon><HomeIcon /></ListItemIcon>
+                  <ListItemText primary={'Home'} />
+                </Link>
+            </ListItem>
+
+            <ListItem button key={1}>
+              <Link to='/creator'>
+                <ListItemText primary={'Creator'} />
               </Link>
-          </ListItem>
+            </ListItem>
+          </List>
+        </Drawer>
 
-          <ListItem button key={1}>
-            <Link to='/creator'>
-              <ListItemText primary={'Creator'} />
-            </Link>
-          </ListItem>
-        </List>
-      </Drawer>
-
-      <Switch>
-        <Route path="/creator">
-          <Creator />
-        </Route>
-        <Route path='/executor'>
-          <Executor />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
+        <Switch>
+          <Route path="/creator">
+            <Creator />
+          </Route>
+          <Route path='/executor'>
+            <Executor />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </ThemeProvider>
     </HashRouter>
   );
 }
