21c15009a95b81b0e161b5ff4ed2bffb9002eb54
Thomas Laforge <thomas.laforge.38@gmail.com>
2021-02-18 11:15:27 +0100

    add log on updater

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,11 @@ Store.initRenderer()
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
-require('update-electron-app')()
+// require('update-electron-app')()
+require('update-electron-app')({
+  updateInterval: '1 hour',
+  logger: require('electron-log')
+})
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
